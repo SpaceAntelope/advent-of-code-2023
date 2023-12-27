c074a2cd43f19e578dca4de75f152ecf1976e9a6
Ares Lazarus <cernunos_kav@msn.com>
2023-12-27 20:40:24 +0200

    added full tilt in place

--- day14/rocks-common.fsx
+++ day14/rocks-common.fsx
@@ -1,22 +1,32 @@
 open System
 open System.Collections.Generic
 
+type Dir = N | E | S | W
+
 let parse (data:string) =
     data.Split('\n', StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
     |> Array.map _.ToCharArray()
     |> array2D
 
 [<TailCall()>]
-let rec indexOfObstacle (line: char[]) (index : int ) =
-    if line[index] <> '.' || index = 0
-    then index 
-    else indexOfObstacle line (index-1)
+let rec indexOfObstacle (line: char[]) (index : int ) (dir: Dir) =
+    // if line[index] <> '.' 
+    // then index, line[index] 
+    // else 
+        match dir with
+        | N | W when index = 0 -> index, line[index]  
+        | N | W when index > 0 && line[index-1] <> '.' -> index-1, line[index-1] 
+        | N | W -> indexOfObstacle line (index-1) dir
+        | S | E when index = line.Length - 1 -> index, line[index]  
+        | S | E when index < line.Length - 1 && line[index+1] <> '.' -> index+1, line[index+1]  
+        | S | E -> indexOfObstacle line (index+1) dir
 
 [<TailCall()>]
 let rec indexOfMovable (line: char[]) (index : int ) =
     if index = line.Length - 1 || line[index] <> '.' 
     then (index,line[index])
     else indexOfMovable line (index+1)
+
 
 let tiltNorth (table: char array2d) =   
     let pointNowDot = HashSet<int*int>()
@@ -48,50 +58,141 @@ let tiltNorth (table: char array2d) =
             else updatedValue
         else updatedValue)    
 
-    // for row in 1..rowCount-1 do
-    //     for col in 0..colCount-1 do
-    //         if table[row,col] = 'O' 
-    //         then 
-    //             let boundaryIndex = indexOfObstacle table[row,0..col] row
-    //             if boundaryIndex < row - 1 then 
-    //                 table[boundaryIndex, col] <- 'O'
-    //                 table[row,col] <- '.'
+let tiltInPlace  (direction: Dir) (table: char array2d) = 
 
-type Dir = N | E | S | W
+    let rowCount = table |> Array2D.length1
+    let colCount = table |> Array2D.length2
+
+    match direction with
+    | N -> 
+        for row in 0..rowCount-1 do
+            for col in 0..colCount-1 do
+                if table[row,col] = 'O' 
+                then 
+                    let obstacleIndex, obstacleItem = indexOfObstacle table[*, col] row direction
+                    // printfn $"%A{table[*, col]} r: {row} c: {col} bi: {obstacleIndex} bitem: {obstacleItem}"
+                    if obstacleIndex = 0 && obstacleItem = '.'
+                    then 
+                        table[0, col] <- 'O' 
+                        table[row,col] <- '.'
+                    else if obstacleIndex < (row - 1) 
+                    then 
+                        table[obstacleIndex+1, col] <- 'O'
+                        table[row,col] <- '.'
+
+                    // printfn $"row: {row} col: {col} value: {table[row,col]}"
+    | W -> 
+        for row in 0..rowCount-1 do
+            for col in 0..colCount-1 do
+                if table[row,col] = 'O' 
+                then 
+                    let obstacleIndex, obstacleItem = indexOfObstacle table[row, *] col direction
+                    // printfn $"%A{table[*, col]} r: {row} c: {col} bi: {obstacleIndex} bitem: {obstacleItem}"
+                    if obstacleIndex = 0 && obstacleItem = '.'
+                    then 
+                        table[row, 0l] <- 'O' 
+                        table[row,col] <- '.'
+                    else if obstacleIndex < (col - 1) 
+                    then 
+                        table[row, obstacleIndex+1] <- 'O'
+                        table[row,col] <- '.'
+
+                    // printfn $"row: {row} col: {col} value: {table[row,col]}"
+    | S -> 
+        for row in rowCount-1..-1..0 do
+            for col in 0..colCount-1 do
+                if table[row,col] = 'O' 
+                then 
+                    let obstacleIndex, obstacleItem = indexOfObstacle table[*,col] row direction
+                    if obstacleIndex = (rowCount - 1) && obstacleItem = '.'                
+                    then  
+                        table[rowCount-1, col] <- 'O' 
+                        table[row,col] <- '.'
+                    else if obstacleIndex > row + 1 
+                    then
+                        table[obstacleIndex-1, col] <- 'O'
+                        table[row,col] <- '.'
+    
+    | E -> 
+        for row in 0..rowCount-1 do
+            for col in colCount-1..-1..0 do
+                if table[row,col] = 'O' 
+                then 
+                    // printfn $"R: {row} C: {col} D: {direction}"
+                    // printf "\tBefore: %A" table[row,*]
+                    let obstacleIndex, obstacleItem = indexOfObstacle table[row,*] col direction
+                    // printfn $"\t {obstacleIndex},{obstacleItem}" 
+                    if obstacleIndex = (colCount - 1) && obstacleItem = '.'                
+                    then  
+                        table[row, colCount-1] <- 'O' 
+                        table[row,col] <- '.'
+                    else if obstacleIndex > col + 1 
+                    then
+                        table[row, obstacleIndex-1] <- 'O'
+                        table[row,col] <- '.'
+                    // printfn "\t After: %A" table[row,*]
+    
+    table
+
+[<TailCall()>]
+let rec indexOfMovableNorth (line: char[]) (index : int ) =
+    if index = line.Length - 1 || line[index] <> '.' 
+    then (index,line[index])
+    else indexOfMovable line (index+1)
+
+[<TailCall()>]
+let rec indexOfMovableSouth (line: char[]) (index: int) =
+    if index = 0 || line.[index] <> '.'
+    then (index, line.[index])    
+    else indexOfMovableSouth line (index-1)
+
 
 let tilt (direction: Dir) (table: char array2d) =
     let pointNowDot = HashSet<int*int>()
     let pointNowRock =HashSet<int*int>()
-    let updateLine (line : char[])  colIndex =
+    let updateCol (line : char[])  colIndex =
         line |> Array.mapi (fun i value -> 
             if pointNowDot.Contains(i,colIndex)
             then '.'
             else if pointNowRock.Contains(i,colIndex)
             then 'O'
             else value)
+    let updateRow (line : char[])  rowIndex =
+        line |> Array.mapi (fun i value -> 
+            if pointNowDot.Contains(rowIndex, i)
+            then '.'
+            else if pointNowRock.Contains(rowIndex,i)
+            then 'O'
+            else value)
+    let updateValue row col value = 
+        if pointNowDot.Contains(row,col) then '.'
+        else if pointNowRock.Contains(row,col) then 'O'
+        else value
 
     table
     |> Array2D.mapi (fun row col value -> 
-        let updatedValue = 
-            if pointNowDot.Contains(row,col) then '.'
-            else if pointNowRock.Contains(row,col) then 'O'
-            else value
-
+        let updatedValue = updateValue row col value 
         if updatedValue = '.'
         then 
-            let updatedRow = 
+            let updatedVector = 
                 match direction with
-                | N -> updateLine table[*,col] col
-                | S -> updateLine table[*,col] col |> Array.rev
-                | E -> updateLine table[row,*] row
-                | W -> updateLine table[row,*] row |> Array.rev
+                | N | S -> updateCol table[*,col] col
+                | E | W -> updateRow table[row,*] row
                 
-            let boundaryIndex,boundaryValue = indexOfMovable updatedRow row
+            let boundaryIndex,boundaryValue = 
+                match direction with 
+                | N -> indexOfMovableNorth updatedVector row
+                | S -> indexOfMovableSouth updatedVector row
 
             if boundaryValue = 'O'
-            then 
-                pointNowDot.Add((boundaryIndex,col)) |> ignore
-                pointNowRock.Add(row,col) |> ignore
+            then
+                match direction with
+                | N -> pointNowDot.Add(boundaryIndex,col) |> ignore
+                | S -> pointNowDot.Add(boundaryIndex,col) |> ignore
+                | E | W -> 
+                    pointNowDot.Add(row,boundaryIndex) |> ignore
+                
+                pointNowRock.Add(row,col) |> ignore                
                 'O'
             else updatedValue
         else updatedValue)    
--- day14/rocks-test.fsx
+++ day14/rocks-test.fsx
@@ -18,6 +18,9 @@ O.#..O.#.#
 #OO..#...."
 
 
+
+
+
 let testTiltNorth = 
     let expectedTable = parse @"OOOO.#.O..
 OO..#....#
@@ -41,3 +44,126 @@ O..#.OO...
     |> function
     | actual when expectedScore = actual -> printfn "Score calcualtion passed"
     | actual -> failwith $"Score calculation failed: Expected {expectedScore} but got {actual}."
+
+
+type TestCase = { Col: char[]; ExpectedS: (int*int*char)[]; ExpectedN: (int*int*char)[] }
+let testIndexOfObstacle1 = 
+    let testCases = [
+        {   Col = [|'O'; 'O'; '.'; 'O'; '.'; 'O'; '.'; '.'; '#'; '#'|] 
+            ExpectedN = [|0,0,'O';1,0,'O'; 2,1,'O';3,1,'O';4,3,'O';5,3,'O';6,5,'O';7,5,'O';8,5,'O';9,8,'#' |]
+            ExpectedS = [|0,1,'O';1,3,'O'; 2,3,'O';3,5,'O';4,5,'O';5,8,'#';6,8,'#';7,8,'#';8,9,'#';9,9,'#' |]
+        }; {
+            Col =  [|'.'; 'O'; '.'; '.'; '.'; '#'; 'O'; '.'; '.'; 'O'|]
+            ExpectedN = [|0,0,'.';1,0,'.'; 2,1,'O';3,1,'O';4,1,'O';5,1,'O';6,5,'#';7,6,'O';8,6,'O';9,6,'O' |]
+            ExpectedS = [|0,1,'O';1,5,'#'; 2,5,'#';3,5,'#';4,5,'#';5,6,'O';6,9,'O';7,9,'O';8,9,'O';9,9,'O' |]
+        };{
+            Col = [|'O'; 'O'; '.'; '#'; 'O'; '.'; '.'; '.'; '.'; 'O'|]
+            ExpectedN = [|0,0,'O';1,0,'O'; 2,1,'O';3,1,'O';4,3,'#';5,4,'O';6,4,'O';7,4,'O';8,4,'O';9,4,'O' |]
+            ExpectedS = [|0,1,'O';1,3,'#'; 2,3,'#';3,4,'O';4,9,'O';5,9,'O';6,9,'O';7,9,'O';8,9,'O';9,9,'O' |]
+        }
+    ]
+   
+    let test col index expectedBoundaryIndex expectedBoundaryValue dir =
+        match indexOfObstacle col index dir with
+        | actualBoundaryIndex, actualBoundaryValue 
+            when expectedBoundaryIndex = actualBoundaryIndex && expectedBoundaryValue = actualBoundaryValue ->
+                printfn $"Obstacle discovery {dir} passed for index {index}"
+        | actualBoundaryIndex, actualBoundaryValue ->
+            failwith $"Obstacle discovery {dir} in\n\t%A{col}\nfailed for index {index}: expected {expectedBoundaryIndex},{expectedBoundaryValue} but got {actualBoundaryIndex},{actualBoundaryValue}"
+    
+    testCases 
+    |> List.iteri (fun i testCase ->             
+            printfn "Case %d: %A" (i+1) testCase.Col
+            testCase.ExpectedN
+            |> Array.iter (fun (rowIndex,expectedBoundaryIndex,expectedBoundaryValue) 
+                            -> test testCase.Col rowIndex expectedBoundaryIndex expectedBoundaryValue N)
+            testCase.ExpectedS
+            |> Array.iter (fun (rowIndex,expectedBoundaryIndex,expectedBoundaryValue) 
+                            -> test testCase.Col rowIndex expectedBoundaryIndex expectedBoundaryValue S)    
+            testCase.ExpectedN
+            |> Array.iter (fun (rowIndex,expectedBoundaryIndex,expectedBoundaryValue) 
+                            -> test testCase.Col rowIndex expectedBoundaryIndex expectedBoundaryValue W)
+            testCase.ExpectedS
+            |> Array.iter (fun (rowIndex,expectedBoundaryIndex,expectedBoundaryValue) 
+                            -> test testCase.Col rowIndex expectedBoundaryIndex expectedBoundaryValue E)
+    
+    )
+    
+let testCases = 
+    [
+        N, parse @"OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#...." 
+        S, parse @"
+.....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O
+"
+        E, parse @"
+....O#....
+.OOO#....#
+.....##...
+.OO#....OO
+......OO#.
+.O#...O#.#
+....O#..OO
+.........O
+#....###..
+#..OO#....
+"
+        W, parse @"
+O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#...."
+    ]     
+let testTiltByDirection() = 
+   
+    let test dir expected = 
+        testData
+        |> parse
+        |> tilt dir
+        |> function 
+        | actual when expected = actual -> printfn $"Tilt by direction {dir} test passed"; actual
+        | actual -> failwith $"Tilt by direction {dir} test failed. Expected \n%A{expected}\n but got \n%A{actual}\n"
+        
+    testCases
+    |> List.iter (fun (dir,table) -> test dir table |> ignore)
+
+testData
+|> parse
+|> printfn "%A" 
+
+let testTiltByDirectionInPlace = 
+   
+    let test dir expected = 
+        testData
+        |> parse
+        |> tiltInPlace dir
+        |> function 
+        | actual when expected = actual -> printfn $"Tilt by direction {dir} in place test passed"; actual
+        | actual -> failwith $"Tilt by direction {dir} in place test failed. Expected \n%A{expected}\n but got \n%A{actual}\n"
+        
+    testCases
+    |> List.iter (fun (dir,table) -> test dir table |> ignore)
+
