d41c46c8ad1daa45fed461a7168a9519b9302c49
Ares Lazarus <cernunos_kav@msn.com>
2023-12-28 11:03:44 +0200

    added explanation

--- day14/rocks-2.fsx
+++ day14/rocks-2.fsx
@@ -5,6 +5,12 @@ open System.IO
 open System.Collections.Generic
 open ``Rocks-common``
 
+(*
+    Every arrangement deterministaclly cycles into a next arrangement, so as soon as an arrangement
+    reappears the pattern should repeat. Finding the 1 billionth arrangement is a matter of finding 
+    the repeating pattern and then mapping its index to the 1e9th index.
+*)
+
 let history = ResizeArray<int * int * string>()
 
 let startOfRepetition =
@@ -19,10 +25,9 @@ let startOfRepetition =
         history.Add(i, scr, h)
         discovered.Add(h))
     |> Seq.last
-    |> ignore
+    |> ignore // we ignore the item returned because takewhile stops the index before the repeating pattern is discovered
 
     history |> Seq.last
-// |> fun (i, tab, scr) -> printfn "%d %d" i scr
 
 let repetitionIndex, _, repetitionHash = startOfRepetition
 
@@ -31,7 +36,9 @@ let startOfPatternIndex =
     |> Seq.find (fun (i, scr, h) -> h = repetitionHash)
     |> fun (idx, _, _) -> idx
 
+
 let indexOfOneBillion =
+    // every number is a zero based index so we use 1e9-1 istead of 1e9 so we don't need further conversions down the line
     let patternLength = repetitionIndex - startOfPatternIndex
     (1000000000-1 - repetitionIndex) % patternLength
 
