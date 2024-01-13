8d76b09cf9ad8fd262c86108ee4ef9cdf8bd2049
Ares Lazarus <cernunos_kav@msn.com>
2024-01-14 01:53:15 +0200

    18-1 passed

---
+++ day18/lagoon-1.fsx
@@ -0,0 +1,18 @@
+#load "./lagoon-common.fsx"
+
+open System
+open System.IO
+open ``Lagoon-common``
+
+
+"./puzzle.input"
+|> File.ReadAllText
+|> parse
+|> path
+|> rebasePath
+|> trenchArea
+//|> fun table -> File.WriteAllText("table.html", html table); table
+|> fillTrench
+//|> fun table -> File.AppendAllText("table.html", html table); table
+|> fun table -> table |> Seq.cast<char> |> Seq.countBy id
+|> printfn "Trech holds %A m^3 of lava"
--- day18/lagoon-test.fsx
+++ day18/lagoon-test.fsx
@@ -1,4 +1,6 @@
+#load "./lagoon-common.fsx"
 open System
+open ``Lagoon-common``
 
 let testData =
     @"R 6 (#70c710)
@@ -17,40 +19,67 @@ L 2 (#015232)
 U 2 (#7a21e3)
 "
 
-type Direction =
-    | D
-    | U
-    | L
-    | R
 
-let DirIndex = [ 'D', D; 'U', U; 'L', L; 'R', R ] |> readOnlyDict
+let trenchArea (path:Path) = 
+    let (rowCount,colCount) = pathAreaDimensions path
+    Array2D.init rowCount colCount (fun row col -> if path |> Array.contains (row,col) then '#' else '.')
 
-type Instruction =
-    { Dir: Direction
-      Steps: int
-      Color: string }
+let fillTrench (trench: Trench) =
+    
+    let rowCount = trench |> Array2D.length1
+    let colCount = trench |> Array2D.length2
 
-let parse (data: string) =
-    data.Split('\n', StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
-    |> Array.map (fun line ->
-        let parts = line.Split(' ')
+    for row in 0..rowCount-1 do
+        let mutable sentinel = false
+        for col in 0..colCount-1 do
+            if trench[row,col] = '#' && (col = colCount-1 || trench[row,col+1] <> '#')
+            then sentinel <- not sentinel
+            else if sentinel = true
+            then trench[row,col] <- '#'
+    
+    trench
 
-        { Dir = DirIndex[parts[0][0]]
-          Steps = int parts[1]
-          Color = parts[2].Trim([| '('; ')'; '#' |]) })
+// testData
+// |> parse
+// |> path 
+// |> trenchArea
+// |> fun trench -> printfn "%A\n" trench; trench
+// |> fillTrench
+// |> fun table -> table |> Seq.cast<char> |> Seq.countBy id
+// |> printfn "%A"
 
-let path (instructions: Instruction[]) =
-    instructions
-    |> Array.scan (fun state instr -> 
-        let (row,col) = state
-        let k = 
-            [|1..instr.Steps|]
-            |> Array.map (fun i -> 
-                    match instr.Dir with
-                    | D ->  (row+i, col)
-                    | U -> (row-i, col)
-                    | R -> (row, col+i)
-                    | R -> (row, col-i))
-        k) [|(0,0)|]
+let pathTest = 
+    let expected = 38
+    testData
+    |> parse
+    |> path    
+    |> Array.length
+    |> function 
+    | actual when actual = expected -> printfn "Path creation test passed"
+    | actual -> printfn $"Path creation test failed. Expected path length {expected} but got {actual}"
 
-[1..10] |> List.scan ( fun state current -> state + current) 0
+
+let testDigTrench1 =
+    let expected = 62
+    testData
+    |> parse
+    |> path    
+    |> fillPath
+    |> function 
+    | actual when actual = expected -> printfn "Path fill test 1 passed"
+    | actual -> printfn $"Path fill test 1 failed. Expected path length {expected} but got {actual}"
+
+let testDigTrench2 =
+    let expected = 62
+    testData
+    |> parse
+    |> path    
+    |> rebasePath
+    |> trenchArea
+    |> fillTrench
+    |> Seq.cast<char>
+    |> Seq.filter (fun c -> c = '#')
+    |> Seq.length
+    |> function 
+    | actual when actual = expected -> printfn "Path fill test 2 passed"
+    | actual -> printfn $"Path fill test 2 failed. Expected path length {expected} but got {actual}"
