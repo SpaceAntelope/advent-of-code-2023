f03e2a68095d29362797e773cb9fae6d9d1b9994
Ares Lazarus <cernunos_kav@msn.com>
2025-01-12 22:12:17 +0200

    5-2 redux: No Force Like Brute Force

--- day05/seed-2.fsx
+++ day05/seed-2.fsx
@@ -1,94 +1,94 @@
 open System
 open System.IO
-open System.Collections.Generic
-open System.Collections
-open System.Text.RegularExpressions
+open System.Diagnostics
 
-let data = File.ReadAllLines("./puzzle.input")
-let testData =  
-    File.ReadAllLines("./seed-test.ps1") 
-    |> Array.skip 1 
-    |> Array.take 32 
-    |> Array.map _.Trim()    
-    |> Array.filter (not<<String.IsNullOrWhiteSpace)
+#load "../Global.fsx"
 
-type Range = { 
-    Map: string
-    First: int64
-    Last: int64
-}
+type Range = { Start: int64; Stop: int64 } 
+    with member x.Contains(n: int64) = x.Start <= n && x.Stop >= n
+type MapEntry = { Dst : Range; Src: Range; Length: int64 }
+type MapIndex = { From: string; To: string; Entries: MapEntry array }
+type SeedRange = { From: int64; Length: int64 } 
+    with 
+        member x.Seeds = 
+            seq { for seed in x.From..x.From + x.Length - 1L do yield seed }
 
-type MapEntry ={ Source: Range; Target: Range }
-
-printfn "%A" testData
-
-let parseRanges (data : string[]) =
-    let index = Dictionary<string,ResizeArray<MapEntry>>()
-    let line2num (line:string) = Regex.Split(line,"\\s+") |> Array.map int64
-
-    let mapLineToRange (mapName: string) (line:string) = 
-        line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
-        |> Array.map int64
-        |> fun arr -> {
-            Source = {
-                Map = mapName
-                First = arr.[1]
-                Last = arr.[1] + arr.[2] - 1L }
-            Target = {
-                Map = mapName
-                First = arr.[0]
-                Last = arr.[0] + arr.[2] - 1L
-            }
-        }
-        
-
-    let seeds = 
-        data
+let parse path =
+    let text = path |> File.ReadAllText
+    let seedRanges = 
+        text.Split(Environment.NewLine)
         |> Array.head
-        |> _.Split(' ')
+        |> _.Trim().Split(' ') 
         |> Array.skip 1
+        |> Array.map int64
         |> Array.chunkBySize 2
-        |> Array.map (fun range -> { Map = "seeds"; First = int64 range.[0]; Last = int64 range.[0] + int64 range.[1] - 1L })
-    
-       
-    data
-    |> Array.skip 1    
-    |> Array.fold (fun (idx: Dictionary<string,ResizeArray<MapEntry>>,currentMapName) line ->
-        match line with
-        | line when line.EndsWith("map:") -> 
-            let newMapName = line.Replace(" map:", "")
-            idx.Add(newMapName, ResizeArray<MapEntry>())
-            idx, newMapName
-        | line when Regex.IsMatch(line, @"(\d+\s*)+") ->
-            idx.[currentMapName].Add(mapLineToRange currentMapName line)
-            idx, currentMapName
-        | line -> failwith $"Can't handle line '{line}'"
+        |> Array.map (fun arr -> { From = arr.[0]; Length = arr.[1] })
+
+    let maps = 
+        text.Split($"{Environment.NewLine}{Environment.NewLine}")
+        |> Array.skip 1
+        |> Array.map (fun substr ->
+            let lines = substr.Split(Environment.NewLine)
+            let src,dst = 
+                lines.[0].Split(' ').[0]
+                |> _.Split("-to-") 
+                |> fun arr -> arr.[0], arr.[1]
+            
+            {
+                From = src
+                To = dst
+                Entries = 
+                    lines 
+                    |> Array.skip 1
+                    |> Array.map (fun line -> 
+                        let nums = line.Split(' ')
+                        let dstStart = int64 nums.[0]
+                        let srcStart = int64 nums.[1]
+                        let length = int64 nums.[2]
+                        {
+                            Dst = { Start = dstStart; Stop = dstStart + length }
+                            Src = { Start = srcStart; Stop = srcStart + length }
+                            Length = int64 nums[2]
+                        })
+            })
+
+    seedRanges, maps
+
+let seedToLocationFactory (maps : MapIndex[]) =
+    let mapSrcIndex = 
+        maps |> Array.map (fun m-> m.From, m ) |> readOnlyDict
+
+    let rec nextMap current (map: MapIndex) = 
+        let target = 
+            map.Entries 
+            |> Array.tryFind (fun e -> e.Src.Contains(current))
+            |> Option.map (fun e -> e.Dst.Start + current - e.Src.Start  )
+            |> function
+            | Some x -> x
+            | None -> current
+
+        if map.To = "location" then target
+        else nextMap target mapSrcIndex.[map.To]
+
+    fun seed -> nextMap seed mapSrcIndex.["seed"]
+
+
+let minLocation path = 
+    let seeds, maps = parse path
+    let seedToLocation = seedToLocationFactory maps
+    seeds
+    |> Array.map (fun seed -> 
+        let sw = Stopwatch()
+        sw.Start()
+        let result = seed.Seeds |> Seq.map seedToLocation |> Seq.min
+        printfn "%A finished at %A" seed sw.Elapsed
+        result )
+    |> Array.min
         
-        ) (index, String.Empty)
-    |> fst
+"./puzzle.example"
+|> minLocation 
+|> Global.shouldBe 46L
 
-
-let index = parseRanges testData 
-let supplyChain = [|"seed-to-soil"; "soil-to-fertilizer"; "fertilizer-to-water"; "water-to-light" ;"light-to-temperature"; "temperature-to-humidity"; "humidity-to-location"|]
-
-index |> printfn "%A"
-index.["seed-to-soil"] |> printfn "%A"
-
-let nextMapIndex = 
-    index.Keys 
-    |> Seq.fold (fun idx mapName -> 
-        let parts = mapName.Split('-')
-        index.Keys 
-        |> Seq.tryFind (fun x -> x.StartsWith(parts.[2]))
-        |> function
-        | Some nextMap -> idx |> Map.add mapName nextMap
-        | None -> idx
-    ) Map.empty
-
-printfn "%A" nextMapIndex
-
-let search (node: MapEntry) = 
-    let mapParts = node.Source.Map.Split('-')    
-    let nextMap = Map.tryFindKey nextMapIndex.[mapParts[2]] index
-    
-    let nextRange = nextMap
+"./puzzle.input"
+|> minLocation 
+|> printfn "The lowest location number that corresponds to any of the initial seed numbers is %i"
