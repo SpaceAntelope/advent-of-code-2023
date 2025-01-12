3f7296f731d413c7a736662c4e4279ece6a558df
Ares Lazarus <cernunos_kav@msn.com>
2025-01-12 23:14:09 +0200

    12-2 redux

--- day12/springs-2.fsx
+++ day12/springs-2.fsx
@@ -1,4 +1,5 @@
 #load "./springs-common.fsx"
+#load "../global.fsx"
 
 
 open System
@@ -10,19 +11,26 @@ open System.Diagnostics
 open System.Threading.Tasks
 open System.Text.RegularExpressions
 
+// let parse path =
+//     path
+//     |> File.ReadAllLines
+// type Spring = {
+//     Mask : string
+//     CRC: int[]
+// }
 
-
-
+let unfold (spring: Spring) =
+    { Mask = Array.create 5 spring.Mask |> Array.reduce (sprintf "%s?%s")
+      CRC = Array.replicate 5 spring.CRC |> Array.collect id }
 
 "./puzzle.input"
 |> File.ReadAllText
 |> parse
-|> Array.sortBy (fun x -> Regex.Matches(x.Mask, "\?").Count)
 |> Array.map unfold
+// |> Array.sortBy (fun x -> Regex.Matches(x.Mask, "\?").Count)
 |> Array.chunkBySize 50
-|> Array.mapi (fun chunkIndex springs -> 
-    async {
-        return springs 
+|> Array.Parallel.mapi (fun chunkIndex springs -> 
+        springs 
         |> Array.mapi (fun i s -> i,s)
         |> Array.sumBy (fun (i,spring) -> 
             let sw = Stopwatch()
@@ -32,8 +40,6 @@ open System.Text.RegularExpressions
             sw.Stop() 
             printfn $"Case %d{chunkIndex * 50 + i} %A{spring.Mask} finished, found {count} arrangements in {sw.Elapsed} at %0.3f{float count/sw.Elapsed.TotalSeconds} per sec"
             count)        
-    })
-|> Async.Parallel
-|> Async.RunSynchronously 
+    )
 |> Array.sum
 |> printfn "Total count of possible arrangements is %d"
