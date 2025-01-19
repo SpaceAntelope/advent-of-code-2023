ef5ee5508e120f22321291eb7a516901a825a8a1
Ares Lazarus <cernunos_kav@msn.com>
2025-01-20 00:48:03 +0200

    17-2 wip

--- day17/Part2.fs
+++ day17/Part2.fs
@@ -1,28 +1,13 @@
 namespace Day17
 
-module Part1 =
+module Part2 =
+
     open System
     open System.IO
     open System.Collections.Generic
     open System.Text.RegularExpressions
     open Common
 
-    let parse path =
-        path
-        |> File.ReadAllLines
-        |> Array.map (fun row ->
-            row.ToCharArray()
-            |> Array.map (fun c -> int (c - '0')))
-        |> array2D
-
-    let printVisited (matrix: Matrix) =
-        let rows, cols = Matrix.size matrix
-
-        for row in 0 .. rows - 1 do
-            for col in 0 .. cols - 1 do
-                printf "%d " matrix.[row, col]
-
-            printfn ""
 
     let arrow (row1, col1) (row2, col2) =
         match row2 - row1, col2 - col1 with
@@ -32,17 +17,25 @@ module Part1 =
         | rowDiff, colDiff when rowDiff < 0 && colDiff = 0 -> '▲'
         | x -> failwithf "%A -> %A = %A bad diff" x (row1, col1) (row2, col2)
 
-    let neighborhood (cell: Cell) =
-        let row, col = cell
 
-        [ row + 1, col
-          row - 1, col
-          row, col + 1
-          row, col - 1 ]
 
     let DykeBuilder (matrix: Matrix) = 
         let rows, cols = matrix |> Matrix.size
         let dirs = 4
+        let minConsecutiveSameDirMoves, maxConsecutiveSameDirMoves = 4,10
+        // consecutive is 4 due to starting from zero
+        // let path = 
+        //     matrix 
+        //     |> Matrix.allIndices 
+        //     |> Seq.fold (fun (state: Dictionary<Cell,Cell*Dir>) current -> 
+        //         state.Add(current, ((-1,-1),Lt)); state ) (Dictionary<Cell,Cell*Dir>())
+        let neighborhood (cell: Cell) =
+            let row, col = cell
+
+            [   row + minConsecutiveSameDirMoves, col
+                row - minConsecutiveSameDirMoves, col
+                row, col + minConsecutiveSameDirMoves
+                row, col - minConsecutiveSameDirMoves ]
 
         let isInBounds row col =
             (Matrix.isOutOfBounds (rows, cols) (row, col))
@@ -51,9 +44,7 @@ module Part1 =
         let initial = 0,0
         let final = rows-1, cols-1
         let visited = 
-            (*  Consecutive count is 4 due to starting from zero, so no need to -1 
-                the actual number everytime the cache is updated *)
-            let v = Array4D.create rows cols dirs 4 Int32.MaxValue
+            let v = Array4D.create rows cols dirs maxConsecutiveSameDirMoves Int32.MaxValue
             v.[0,0,Lt.AsInt,0] <- 0
             v
         let toVisit = 
@@ -62,7 +53,7 @@ module Part1 =
             q
 
         let rec search (state : State) = 
-            if  state.Current = final 
+            if state.Current = final 
             then visited.[state.Row,state.Col,state.Dir.AsInt,state.Repeats]
             else             
                 let n = 
@@ -73,14 +64,24 @@ module Part1 =
                     let nextDir = Dir.From(state.Current, (row,col))
                     let nextRepeatedDir = 
                         if nextDir = state.Dir 
-                        then state.Repeats + 1 else 1
+                        then state.Repeats + 1 else 4
 
                     let isDoublingBack = nextDir.Opposite() = state.Dir
 
-                    if nextRepeatedDir <= 3 && not isDoublingBack
+                    if nextRepeatedDir <= 10 && not isDoublingBack
                     then
                         let lastScore = visited.[state.Row,state.Col,state.Dir.AsInt,state.Repeats]
-                        let nextScore = lastScore + matrix.[row,col]    
+                        let nextScore = 
+                            if nextRepeatedDir = 4
+                            then 
+                                match nextDir with
+                                | Rt -> matrix.[row, col-4..col]
+                                | Lt -> matrix.[row, col..col+4]
+                                | Up -> matrix.[row-4..row, col]
+                                | Dn -> matrix.[row..row+4, col]
+                                |> Array.sum 
+                                |> (+) lastScore
+                            else lastScore + matrix.[row,col]
 
                         // if row = rows-1 && col = cols-1
                         // then 
@@ -95,4 +96,24 @@ module Part1 =
 
         toVisit.Dequeue() |> search
 
-        
+
+
+        // let cellsOfPath = 
+        //     [
+        //         let mutable parent = path.[rows-1,cols-1]
+        //         yield parent
+        //         while fst parent <> (0,0) do
+        //             printfn "%A" parent
+        //             let (row,col), dir = parent
+        //             parent <- path.[row,col]
+        //             yield parent
+        //     ] 
+        //     |> Map.ofList
+
+        // let pathMatrix = Array2D.init rows cols (fun row col -> 
+        //     match cellsOfPath |> Map.tryFind (row,col) with
+        //     | Some x -> x.ToArrow()
+        //     | None -> '0' + char matrix.[row,col])
+
+        // Matrix.printBase pathMatrix []
+
--- day17/Program.fs
+++ day17/Program.fs
@@ -5,13 +5,38 @@
 // printfn "Hello from F#"
 open Day17
 open Common
+open System.IO
+
+let parse path =
+    path
+    |> File.ReadAllLines
+    |> Array.map (fun row ->
+        row.ToCharArray()
+        |> Array.map (fun c -> int (c - '0')))
+    |> array2D
+
 
 "./Input/puzzle.example"
-|> Part1.parse
+|> parse
 |> Part1.DykeBuilder
 |> Assertions.shouldBe 102
 
 "./Input/puzzle.input"
-|> Part1.parse
+|> parse
 |> Part1.DykeBuilder
 |> printfn "The least heat loss the crucible can incur is %d"
+
+"./Input/puzzle.example"
+|> parse
+|> Part2.DykeBuilder
+|> Assertions.shouldBe 94
+
+"./Input/puzzle.example2"
+|> parse
+|> Part2.DykeBuilder
+|> Assertions.shouldBe 71
+
+"./Input/puzzle.input"
+|> parse
+|> Part2.DykeBuilder
+|> printfn "The least heat loss the ultra crucible can incur is %d"
