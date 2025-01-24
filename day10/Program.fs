c106237e29b19654feb4afc1cf76848f96bb8b7b
Ares Lazarus <cernunos_kav@msn.com>
2025-01-24 09:01:54 +0200

    10-2 wip

--- day10/Program.fs
+++ day10/Program.fs
@@ -1,7 +1,18 @@
 ﻿open System.IO
 open Common
 
-type Dir = N | E | W | S
+type Dir = Lt | Rt | Up | Dn
+    with 
+        static member From(cell1: int*int, cell2: int*int) =
+            let row1,col1 = cell1
+            let row2,col2 = cell2
+            match row2-row1,col2-col1 with
+            | rowDiff, colDiff when rowDiff = 0 && colDiff > 0 -> Rt
+            | rowDiff, colDiff when rowDiff = 0 && colDiff < 0 -> Lt
+            | rowDiff, colDiff when rowDiff > 0 && colDiff = 0 -> Dn
+            | rowDiff, colDiff when rowDiff < 0 && colDiff = 0 -> Up
+            | x -> failwithf "%A -> %A = %A bad diff" x (row1,col1) (row2,col2)
+
 type Tile = 
     | Hor | Ver | TLt | TRt | BLt | BRt | Ground | Start | Enclosed
     with
@@ -52,30 +63,95 @@ let printTiles (matrix: Tile array2d) =
 |> List.map parse
 |> List.iter printTiles
 
-// let followPipe matrix =
-//     let isOutOfBounds = Matrix.isOutOfBoundsFactory matrix
-//     let start = matrix |> Matrix.findCell (fun tile -> tile = S)
-//     let visited = Set.empty<int*int>
-//     let next cell = 
-        
-//         [   row + 1, col
-//             row - 1, col
-//             row, col + 1
-//             row, col - 1 ]
-//         |> List.filter (fun (row,col) -> not isOutOfBounds row col && not visited |> Set.contains((row,col)) )
-//         |> List.find (fun candidate ->
-//                 let row1, col1 = cell
-//                 let row2, col2 = candidate
-//                 match matrix.[row1,col1], matrix.[row2,col2] with
-//                 | Hor, Hor
-//                 | Ver, Ver -> true
-//                 | TLt, TRt -> true
-//                 | BLt, BRt -> true
-//                 | TLt
 
-//         )
-//         match matrix.[row,col], 
-//     let rec search current = 
+let followPipe matrix =
+    let isOutOfBounds = Matrix.isOutOfBoundsFactory matrix
+    let start = matrix |> Matrix.findCell (fun tile -> tile = Tile.Start)
+    let mutable visited = Set.empty<int*int>
+    let next cell = 
+        let row,col = cell
+        [   row + 1, col
+            row - 1, col
+            row, col + 1
+            row, col - 1 ]
+        |> List.filter (fun (row,col) -> not (isOutOfBounds row col) && not (visited |> Set.contains((row,col))) )
+        |> List.tryFind (fun candidate ->
+                let row1, col1 = cell
+                let row2, col2 = candidate
+                let dir = Dir.From(cell, candidate)
+                
+                match matrix.[row1,col1], matrix.[row2,col2], dir with
+                | Hor, Hor, Rt -> true // ──
+                | Hor, Hor, Lt -> true // ──
+                
+                | Hor, BRt, Rt -> true // ─┘
+                | Hor, TRt, Rt -> true // ─┐
+                | BLt, Hor, Rt -> true // └─
+                | BLt, BRt, Rt -> true // └┘
+                | BLt, TRt, Rt -> true // └┐
+                | TLt, Hor, Rt -> true // ┌─
+                | TLt, BRt, Rt -> true // ┌┘
+                | TLt, TRt, Rt -> true // ┌┐
+                
+                | BRt, Hor, Lt -> true // ─┘
+                | TRt, Hor, Lt -> true // ─┐
+                | Hor, BLt, Lt -> true // └─
+                | BRt, BLt, Lt -> true // └┘
+                | TRt, BLt, Lt -> true // └┐
+                | Hor, TLt, Lt -> true // ┌─
+                | BRt, TLt, Lt -> true // ┌┘
+                | TRt, TLt, Lt -> true // ┌┐
+
+                //  │
+                //  │
+                | Ver, Ver, Dn -> true
+                | Ver, Ver, Up -> true
+                
+                //  │
+                //  └
+                | Ver, BLt, Dn -> true
+                //  │
+                //  ┘
+                | BRt, TLt, Dn -> true
+                //  ┐
+                //  │
+                | TRt, Ver, Dn -> true
+                //  ┐
+                //  └
+                | TRt, BLt, Dn -> true
+                //  ┐
+                //  ┘
+                | TRt, BRt, Dn -> true
+                //  ┌
+                //  │
+                | TLt, Ver , Dn-> true
+                //  ┌
+                //  └
+                | TLt, BLt, Dn -> true
+                //  ┌
+                //  ┘
+                | TLt, BRt, Dn -> true
+                
+                | BLt, Ver, Up -> true
+                | TLt, BRt, Up -> true
+                | Ver, TRt, Up -> true
+                | BLt, TRt, Up -> true
+                | BRt, TRt, Up -> true
+                | Ver, TLt, Up-> true
+                | BLt, TLt, Up -> true
+                | BRt, TLt, Up -> true
+                | _ -> false)
+                                
+    let rec search current = 
+        visited <- visited |> Set.add current
+        [ 
+            yield! 
+                match (next current) with
+                | None -> []
+                | Some cell -> [Some current; next cell]
+        ] |> List.choose id
+    
+    search start
 
 let tileSet = [
     Ver
@@ -85,10 +161,17 @@ let tileSet = [
     TRt
     TLt]
 
+printfn "Horizontal"
 tileSet 
 |> List.allPairs tileSet 
 |> List.iter (fun (t1,t2) -> 
+    printfn "| %A, %A -> true // %c%c" t1 t2 t1.AsAscii t2.AsAscii)
+
+printfn "Vertical"
+tileSet 
+|> List.allPairs tileSet 
+|> List.iter (fun (t1,t2) -> 
+    printfn "//  %c" t1.AsAscii
+    printfn "//  %c" t2.AsAscii    
     printfn "| %A, %A -> true" t1 t2
-    printfn "%c%c" t1.AsAscii t2.AsAscii
-    printfn "%c\n" t2.AsAscii    
      )
