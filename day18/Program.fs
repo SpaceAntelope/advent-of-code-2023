1389b09d36c359e1215eef302ec34b8e10a77579
Ares Lazarus <cernunos_kav@msn.com>
2025-03-09 14:23:08 +0200

    18-2 wip

--- day18/Program.fs
+++ day18/Program.fs
@@ -41,4 +41,42 @@ module Main =
                 Hex = parts.[2].Trim([|'#';'(';')'|]) })
         |> Array.map _.Corrected()
 
-    parse "./input/puzzle.example" |> Array.iter (printfn "%A")
+    let pathToPoints (data: (Dir*int64) array) =
+        data 
+        |> Array.scan (fun (row,col) (dir,distance) -> 
+                match dir with 
+                | Dn -> row + distance, col
+                | Up -> row - distance, col
+                | Lt -> row, col - distance
+                | Rt -> row, col + distance) (0L,0L)
+        |> Array.skip 1
+
+    let size (data: (Dir*int64) array) =
+        Array.fold (fun (rows,cols) (dir,dist) -> 
+            printfn "%A" (rows,cols,dir,dist)
+            match dir with 
+            | Dn -> rows + dist, cols
+            | Up -> rows - dist, cols
+            | Lt -> rows, cols - dist
+            | Rt -> rows, cols + dist) (0L,0L) data
+        
+
+    
+
+    // let calculateArea (data: (int64*int64) array) = 
+    //     data
+    //     |> Array.groupBy fst        
+    //     |> Array.map (fun (key,grp)-> key, grp |> Array.sortBy snd)
+    //     |> Array.sortBy fst
+    //     |> Array.map snd
+    //     |> Array.fold (fun (prevRows, area) currentRowPoints -> 
+
+    //     ) ([||],0L)
+
+    //     |> Array.iter (printfn "%A")
+
+    "./input/puzzle.example" 
+    |> parse     
+    |> pathToPoints
+    |> calculateArea
+    // |> Array.iter (printfn "%A")        
